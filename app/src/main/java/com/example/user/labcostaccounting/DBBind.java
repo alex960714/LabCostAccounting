39c13788711a69ab09a1b76ccc3f701d98ab3e49
alex960714 <alex_9607@mail.ru>
2017-12-17 02:50:32 +0300

    Backend part init

---
+++ app/src/main/java/com/example/user/labcostaccounting/DBBind.java
@@ -0,0 +1,4 @@
+package com.example.user.labcostaccounting;
+
+public class DBBind {
+}
--- app/src/main/java/com/example/user/labcostaccounting/MainActivity.java
+++ app/src/main/java/com/example/user/labcostaccounting/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.user.labcostaccounting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addBtn;
+    private Button delBtn;
+    private Button totalBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addBtn=(Button)findViewById(R.id.addButton);
+        delBtn=(Button)findViewById(R.id.deleteButton);
+        totalBtn =(Button)findViewById(R.id.totalButton);
     }
 }
